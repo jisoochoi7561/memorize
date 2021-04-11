2a3ede7b1140d4481ccd98cbff0ed31f9ae38821
jisoo <jisoochoi7561@gmail.com>
2021-04-11 17:47:24 +0900

    finished lecture1

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojiGame :EmojiMemoryGame
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            ForEach(emojiGame.cards){card in 
+                CardView(card: card).onTapGesture {
+                    emojiGame.choose(card: card)
+                }
+            }
+        }
+                .padding()
+                .foregroundColor(.orange)
+                .font(.largeTitle)
+        
+        
     }
 }
 
+
+struct CardView:View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View{
+        ZStack{
+            if card.isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text(card.content)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(emojiGame:EmojiMemoryGame())
     }
 }
--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -14,9 +14,22 @@ struct MemoryGame<CardContent> {
         print("card chosen: \(card)")
     }
     
-    struct Card {
-        var isFaceUp:Bool
-        var isMatched:Bool
+    
+    init(numberOfPairs:Int,factory:(Int)->CardContent) {
+        cards = Array<Card>()
+        for index in 0..<numberOfPairs{
+            let content = factory(index)
+            cards.append(Card(isFaceUp: true, isMatched: false, content: content,id:index*2))
+            cards.append(Card(isFaceUp: true, isMatched: false, content: content,id:index*2+1))
+  
+        }
+    }
+    
+    
+    struct Card:Identifiable {
+        var isFaceUp:Bool = false
+        var isMatched:Bool = false
         var content: CardContent
+        var id: Int
     }
 }
